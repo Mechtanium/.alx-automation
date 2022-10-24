bf5b44b5f1899310c7ce9d12a5dd476a38643a71
Mechtanium <A.sapiens.view@gmail.com>
2022-10-24 11:52:45 +0100

    v4.0.0

--- alias.sh
+++ alias.sh
@@ -5,5 +5,5 @@ alias u="./.alx-automation/update.sh"
 alias v="echo \"v4.0.0\""
 alias t="./.alx-automation/tests.sh"
 alias >> ~/.bash_aliases
-
+# Create man page
 cp ./alx-auto.1 /usr/local/man/man1/alx-auto.1
