adab88fdc41edc4a08ac14e9a878b73d584cca7f
Mechtanium <A.sapiens.view@gmail.com>
2022-10-24 11:27:21 +0100

    v.4.0.0

--- alias.sh
+++ alias.sh
@@ -4,6 +4,6 @@ alias d="source ./.alx-automation/uninstall.sh"
 alias u="./.alx-automation/update.sh"
 alias v="echo \"v4.0.0\""
 alias t="./.alx-automation/tests.sh"
-alias > ~/.bash_aliases
+alias >> ~/.bash_aliases
 
 cp ./.alx-automation/alx-auto.1 /usr/local/man/man1/alx-auto.1
