03b601926809e902f3fdb2eaaf2f0b47e9a0020c
Enosolease M. Ame <enosmath@gmail.com>
2022-10-25 13:43:56 -0700

    Use absolute path for working directory variable

--- engagement.sh
+++ engagement.sh
@@ -11,7 +11,7 @@ elif [[ $1 == '-u' ]]
 then
 	u
 else
-	echo "export DIR_PATH=\"./$1\"" >> ~/.bashrc
+	echo "export DIR_PATH=\"$(pwd)/$1\"" >> ~/.bashrc
 	source ~/.bashrc
 	echo "Working directory changed to $DIR_PATH"
 fi
